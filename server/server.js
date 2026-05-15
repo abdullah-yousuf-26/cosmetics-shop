cefde1d004d3647fe84cdf9401c8bb118b50f724
Yousuf <149528624+abdullah-yousuf-26@users.noreply.github.com>
2026-05-15 19:07:38 +0600

    new link
    https://cosmetics-shop-wheat.vercel.app/

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000', 
-        'https://cosmetics-shop-3zwt.vercel.app'
+        'https://cosmetics-shop-wheat.vercel.app/'
     ], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
