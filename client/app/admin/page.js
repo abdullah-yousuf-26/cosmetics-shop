e5794a3948b6818df7c6f18e7d3990c073ff4660
Yousuf <149528624+abdullah-yousuf-26@users.noreply.github.com>
2026-05-15 23:40:31 +0600

    fetchUsers
    fetchUsers update

--- client/app/admin/page.js
+++ client/app/admin/page.js
@@ -95,7 +95,8 @@ export default function AdminPage() {
         try {
             if (!userInfo?.token) return;
             const config = { headers: { Authorization: `Bearer ${userInfo.token}` } };
-            const { data } = await axios.get(`${API_URL}/api/orders`, config);
+            //  Change /orders to /users
+            const { data } = await axios.get(`${API_URL}/api/users`, config); 
             setUsers(data);
         } catch (err) { console.error("User fetch failed."); }
     };
